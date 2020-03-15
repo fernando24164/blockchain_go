f0a0b20d52824787e827a76ec067763fdde304a5
Fernando <fernando24164@gmail.com>
2020-03-15 19:07:23 +0100

    Add persistence to blockchain

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -2,21 +2,80 @@ package blockchain
 
 import (
 	"blockchain/block"
+
+	"github.com/boltdb/bolt"
 )
+
+const dbFile = "blockchain.db"
+const blocksBucket = "blocks"
 
 // Blockchain struct
 type Blockchain struct {
-	Blocks []*block.Block
+	tip []byte
+	db  *bolt.DB
 }
 
 // AddBlock method to add a block to blockchain
 func (bc *Blockchain) AddBlock(data string) {
-	prevBlock := bc.Blocks[len(bc.Blocks)-1]
-	newBlock := block.NewBlock(data, prevBlock.Hash)
-	bc.Blocks = append(bc.Blocks, newBlock)
+	var lastHash []byte
+
+	err := bc.db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(blocksBucket))
+		lastHash = bucket.Get([]byte("l"))
+
+		return nil
+	})
+
+	if err != nil {
+		panic("Porblem retrieving info from database")
+	}
+
+	newBlock := block.NewBlock(data, lastHash)
+
+	_ = bc.db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(blocksBucket))
+		err := bucket.Put(newBlock.Hash, newBlock.Serialize())
+		err = bucket.Put([]byte("l"), newBlock.Hash)
+		bc.tip = newBlock.Hash
+
+		if err != nil {
+			panic("Porblem retrieving info from database")
+		}
+
+		return nil
+	})
+
 }
 
 // NewBlockchain create a new blockchain
 func NewBlockchain() *Blockchain {
-	return &Blockchain{[]*block.Block{block.NewGenesisBlock()}}
+	var tip []byte
+	db, err := bolt.Open(dbFile, 0600, nil)
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(blocksBucket))
+
+		if bucket == nil {
+			genesis := block.NewGenesisBlock()
+			b, _ := tx.CreateBucket([]byte(blocksBucket))
+			err = b.Put(genesis.Hash, genesis.Serialize())
+			// Store last block reference
+			err = b.Put([]byte("l"), genesis.Hash)
+			tip = genesis.Hash
+		} else {
+			tip = bucket.Get([]byte("l"))
+		}
+
+		if err == nil {
+			return nil
+		} else {
+			return err
+		}
+
+	})
+
+	bc := Blockchain{tip, db}
+
+	return &bc
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module blockchain
 
 go 1.14
+
+require (
+	github.com/boltdb/bolt v1.3.1
+
+	golang.org/x/sys v0.0.0-20200302150141-5c8b2ff67527 // indirect
+)
